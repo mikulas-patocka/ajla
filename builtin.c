mikulas-patocka/ajla

commit 0b144703b6af5355c3044692fd982142d971920f

invalidate cache when builtin.pcd changes

builtin.c
@@ -21,6 +21,7 @@
 #include "mem_al.h"
 #include "os.h"
 #include "os_util.h"
+#include "save.h"
 
 #include "builtin.h"
 
@@ -138,7 +139,6 @@ void builtin_init(void)
 	builtin_lib_path = pte;
 	builtin_path = os_join_paths(pte, "builtin.pcd", true, NULL);
 	h = os_open(os_cwd, builtin_path, O_RDONLY, 0, &sink);
-	mem_free(builtin_path);
 	if (unlikely(handle_is_valid(h)))
 		goto found_builtin;
 
@@ -146,7 +146,6 @@ void builtin_init(void)
 	builtin_lib_path = AJLA_LIB;
 	builtin_path = os_join_paths(AJLA_LIB, "builtin.pcd", true, NULL);
 	h = os_open(os_cwd, builtin_path, O_RDONLY, 0, &sink);
-	mem_free(builtin_path);
 	if (unlikely(handle_is_valid(h)))
 		goto found_builtin;
 #endif
@@ -171,7 +170,7 @@ found_builtin:
 				builtin_ptr = ptr;
 				builtin_mapped = true;
 				os_close(h);
-				return;
+				goto finalize;
 			}
 		} else if (unlikely(fi.signature != 0x416A6C61))
 			goto bad_sig;
@@ -202,6 +201,9 @@ bad_sig:
 			p[3] = a[0];
 		}
 	}
+finalize:
+	save_register_dependence(builtin_path);
+	mem_free(builtin_path);
 }
 
 void builtin_done(void)

ajla.c
@@ -70,9 +70,9 @@ int main(int argc, const char * const argv[])
 	function_init();
 	pcode_init();
 	profile_init();
-	builtin_init();
 	ipio_init();
 	save_init();
+	builtin_init();
 	module_init();
 	ipret_init();
 	tick_init();
@@ -94,9 +94,9 @@ int main(int argc, const char * const argv[])
 	tick_done();
 	ipret_done();
 	module_done();
+	builtin_done();
 	save_done();
 	ipio_done();
-	builtin_done();
 	profile_done();
 	pcode_done();
 	function_done();

commit c1e36146b130db97bc38d8486fc0054ecb75334a

fix memory leak introduced by 0b144703b6af5355c3044692fd982142d971920f

builtin.c
@@ -145,6 +145,7 @@ void builtin_init(void)
 	h = os_open(os_cwd, builtin_path, O_RDONLY, 0, &sink);
 	if (unlikely(handle_is_valid(h)))
 		goto found_builtin;
+	mem_free(builtin_path);
 
 #ifdef AJLA_LIB
 	builtin_lib_path = AJLA_LIB;