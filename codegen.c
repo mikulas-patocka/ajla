mikulas-patocka/ajla

commit 7ca91a8cf43ba32765cd261b591562fc8ba9429b

implement integer operations with immediate operand
fix a bug on parisc that we had polarity of need_nop reversed

c2-hppa.inc
@@ -627,6 +627,7 @@ static bool attr_w cgen_cmp_dest_reg(struct codegen_context *ctx, unsigned size,
 static bool attr_w cgen_alu(struct codegen_context *ctx, unsigned size, unsigned writes_flags, unsigned alu, bool trap, unsigned length)
 {
 	uint32_t mc;
+	uint8_t z = R_ZERO;
 	uint8_t *arg1 = ctx->code_position;
 	uint8_t *arg2 = arg1 + arg_size(*arg1);
 	uint8_t *arg3 = arg2 + arg_size(*arg2);
@@ -637,6 +638,10 @@ static bool attr_w cgen_alu(struct codegen_context *ctx, unsigned size, unsigned
 
 	if (arg3[0] == ARG_IMM && !trap) {
 		int64_t imm = get_imm(&arg3[1]);
+		if (!imm) {
+			arg3 = &z;
+			goto do_3reg;
+		}
 		if (unlikely(writes_flags))
 			goto invalid;
 		switch (alu) {
@@ -663,6 +668,7 @@ static bool attr_w cgen_alu(struct codegen_context *ctx, unsigned size, unsigned
 		}
 	}
 
+do_3reg:
 	switch (alu) {
 		case ALU_ADD:	mc = writes_flags ? PARISC_ADD : PARISC_ADD_L;
 				break;
@@ -708,13 +714,13 @@ static bool attr_w cgen_alu(struct codegen_context *ctx, unsigned size, unsigned
 
 #ifdef ARCH_PARISC32
 	if (alu == ALU_ADD && size == OP_SIZE_4 && arg1[0] == arg2[0] && trap && length == JMP_SHORTEST) {
-		bool need_nop;
+		bool known;
 		mc = PARISC_ADDB;
 		mc |= PARISC_COND_SV;
 		mc |= PARISC_N;
-		g(add_relocation(ctx, JMP_SHORTEST, ctx->code_position - arg1, &need_nop));
+		g(add_relocation(ctx, JMP_SHORTEST, ctx->code_position - arg1, &known));
 		g(cgen_2reg(ctx, mc, arg1[0], arg3[0]));
-		if (unlikely(need_nop))
+		if (unlikely(!known))
 			cgen_four(PARISC_NOP);
 		return true;
 	}
@@ -1245,11 +1251,11 @@ static bool attr_w cgen_fp_from_int(struct codegen_context *ctx, unsigned int_op
 static bool attr_w cgen_cond_jmp(struct codegen_context *ctx, uint32_t mc, unsigned length, unsigned reg1, unsigned reg2, int reloc_offset)
 {
 	if (length == JMP_SHORTEST) {
-		bool need_nop;
+		bool known;
 		mc |= PARISC_N;
-		g(add_relocation(ctx, JMP_SHORTEST, reloc_offset, &need_nop));
+		g(add_relocation(ctx, JMP_SHORTEST, reloc_offset, &known));
 		g(cgen_2reg(ctx, mc, reg2, reg1));
-		if (unlikely(need_nop))
+		if (unlikely(!known))
 			cgen_four(PARISC_NOP);
 	} else if (length == JMP_SHORT) {
 		mc |= PARISC_N;

codegen.c
@@ -928,6 +928,35 @@ do {									\
 #define IMM_PURPOSE_BITWISE		21
 
 
+static unsigned alu_purpose(unsigned alu)
+{
+	unsigned purpose =
+		alu == ALU_ADD ? IMM_PURPOSE_ADD :
+		alu == ALU_ADC ? IMM_PURPOSE_ADD :
+		alu == ALU_SUB ? IMM_PURPOSE_SUB :
+		alu == ALU_SBB ? IMM_PURPOSE_SUB :
+		alu == ALU_MUL ? IMM_PURPOSE_MUL :
+		alu == ALU_UMULH ? IMM_PURPOSE_MUL :
+		alu == ALU_SMULH ? IMM_PURPOSE_MUL :
+		alu == ALU_ANDN ? IMM_PURPOSE_ANDN :
+		alu == ALU_AND ? IMM_PURPOSE_AND :
+		alu == ALU_OR ? IMM_PURPOSE_OR :
+		alu == ALU_XOR ? IMM_PURPOSE_XOR :
+		alu == ALU_EXTBL ? IMM_PURPOSE_OR :
+		alu == ALU_EXTWL ? IMM_PURPOSE_OR :
+		alu == ALU_EXTLL ? IMM_PURPOSE_OR :
+		alu == ALU_EXTLH ? IMM_PURPOSE_OR :
+		alu == ALU_INSBL ? IMM_PURPOSE_OR :
+		alu == ALU_MSKBL ? IMM_PURPOSE_OR :
+		alu == ALU_ZAP ? IMM_PURPOSE_ANDN :
+		alu == ALU_ZAPNOT ? IMM_PURPOSE_AND :
+		-1U;
+	if (unlikely(purpose == -1U))
+		internal(file_line, "alu_purpose: invalid alu %u", alu);
+	return purpose;
+}
+
+
 static bool attr_w gen_upcall_end(struct codegen_context *ctx, unsigned args);
 
 #define gen_address_offset()						\
@@ -955,6 +984,16 @@ do {									\
 } while (0)
 
 #define is_imm()	(!ctx->const_reg)
+
+
+static inline bool slot_is_register(struct codegen_context *ctx, frame_t slot)
+{
+	if (frame_t_is_const(slot))
+		return false;
+	if (unlikely(slot >= function_n_variables(ctx->fn)))
+		internal(file_line, "slot_is_register: invalid slot %lu", (unsigned long)slot);
+	return ctx->registers[slot] >= 0;
+}
 
 
 #define insn_file		1
@@ -1309,7 +1348,7 @@ static bool attr_w gen_function(struct codegen_context *ctx)
 			code -= OPCODE_INT_OP;
 			op = (code / OPCODE_INT_OP_MULT) % OPCODE_INT_TYPE_MULT;
 			type = code / OPCODE_INT_TYPE_MULT;
-			if (op < OPCODE_INT_OP_UNARY) {
+			if (op < OPCODE_INT_OP_C) {
 				get_two(ctx, &slot_1, &slot_2);
 				get_two(ctx, &slot_r, &flags);
 				escape_label = alloc_escape_label(ctx);
@@ -1319,6 +1358,24 @@ static bool attr_w gen_function(struct codegen_context *ctx)
 				flag_set(ctx, slot_1, false);
 				flag_set(ctx, slot_2, false);
 				flag_set(ctx, slot_r, false);
+				if (flags & OPCODE_FLAG_FUSED) {
+					g(gen_fused_binary(ctx, MODE_INT, type, op, escape_label, slot_1, slot_2, slot_r, &failed));
+					if (unlikely(!failed))
+						continue;
+				}
+				g(gen_alu(ctx, MODE_INT, type, op, escape_label, slot_1, slot_2, slot_r));
+				continue;
+			} else if (op < OPCODE_INT_OP_UNARY) {
+				op -= OPCODE_INT_OP_C;
+				get_two(ctx, &slot_1, &slot_2);
+				get_two(ctx, &slot_r, &flags);
+				escape_label = alloc_escape_label(ctx);
+				if (unlikely(!escape_label))
+					return false;
+				g(gen_test_1_cached(ctx, slot_1, escape_label));
+				flag_set(ctx, slot_1, false);
+				flag_set(ctx, slot_r, false);
+				slot_2 = frame_t_from_const((int32_t)slot_2);
 				if (flags & OPCODE_FLAG_FUSED) {
 					g(gen_fused_binary(ctx, MODE_INT, type, op, escape_label, slot_1, slot_2, slot_r, &failed));
 					if (unlikely(!failed))
@@ -1534,7 +1591,7 @@ unconditional_escape:
 				g(gen_test_1(ctx, R_FRAME, slot_1, 0, label_id, false, TEST_SET));
 do_take_borrowed:
 				g(gen_upcall_start(ctx, 1));
-				g(gen_frame_load(ctx, OP_SIZE_SLOT, garbage, slot_1, 0, R_ARG0));
+				g(gen_frame_load(ctx, OP_SIZE_SLOT, garbage, slot_1, 0, false, R_ARG0));
 				g(gen_upcall_argument(ctx, 0));
 				g(gen_upcall(ctx, offsetof(struct cg_upcall_vector_s, cg_upcall_pointer_reference_owned), 1));
 				flag_set(ctx, slot_1, true);
@@ -1560,7 +1617,7 @@ take_borrowed_done:
 					label_id = 0;	/* avoid warning */
 				}
 				g(gen_upcall_start(ctx, 1));
-				g(gen_frame_load(ctx, OP_SIZE_SLOT, garbage, slot_1, 0, R_ARG0));
+				g(gen_frame_load(ctx, OP_SIZE_SLOT, garbage, slot_1, 0, false, R_ARG0));
 				g(gen_upcall_argument(ctx, 0));
 				g(gen_upcall(ctx, offsetof(struct cg_upcall_vector_s, cg_upcall_pointer_dereference), 1));
 				if (need_bit_test)
@@ -2056,7 +2113,7 @@ static bool attr_w gen_epilogues(struct codegen_context *ctx)
 	}
 	gen_label(escape_label);
 	for (v = MIN_USEABLE_SLOT; v < function_n_variables(ctx->fn); v++) {
-		if (ctx->registers[v] >= 0) {
+		if (slot_is_register(ctx, v)) {
 			g(spill(ctx, v));
 		}
 	}

asm.c
@@ -55,6 +55,11 @@ static void dump_registers(int sig, ucontext_t *uc)
 	for (i = 0; i < 32; i++)
 		debug("gpr_%d = %lx", i, uc->uc_mcontext.regs->gpr[i]);
 #endif
+#if defined(ARCH_S390)
+	debug("%s at %lx", sig == SIGSEGV ? "sigsegv" : sig == SIGBUS ? "sigbus" : "sigill", uc->uc_mcontext.psw.addr);
+	for (i = 0; i < 16; i++)
+		debug("gpr_%d = %lx", i, uc->uc_mcontext.gregs[i]);
+#endif
 #if defined(__SH4__)
 	debug("%s at %x", sig == SIGSEGV ? "sigsegv" : sig == SIGBUS ? "sigbus" : "sigill", uc->uc_mcontext.pc);
 	for (i = 0; i < 16; i++)

c1-alpha.inc
@@ -472,12 +472,19 @@ static bool attr_w gen_upcall_argument(struct codegen_context attr_unused *ctx, 
 	return true;
 }
 
+static bool attr_w gen_get_upcall_pointer(struct codegen_context *ctx, unsigned offset, unsigned reg)
+{
+	g(gen_address(ctx, R_UPCALL, offset, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_ADDRESS));
+	gen_insn(INSN_MOV, OP_SIZE_ADDRESS, 0, 0);
+	gen_one(reg);
+	gen_address_offset();
+
+	return true;
+}
+
 static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsigned n_args)
 {
-	g(gen_address(ctx, R_UPCALL, offset, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_NATIVE));
-	gen_insn(INSN_MOV, OP_SIZE_ADDRESS, 0, 0);
-	gen_one(R_T12);
-	gen_address_offset();
+	g(gen_get_upcall_pointer(ctx, offset, R_T12));
 
 	gen_insn(INSN_CALL_INDIRECT, OP_SIZE_ADDRESS, 0, 0);
 	gen_one(R_T12);

c1-arm.inc
@@ -398,12 +398,19 @@ static bool attr_w gen_upcall_argument(struct codegen_context attr_unused *ctx, 
 	return true;
 }
 
-static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsigned n_args)
+static bool attr_w gen_get_upcall_pointer(struct codegen_context *ctx, unsigned offset, unsigned reg)
 {
 	g(gen_address(ctx, R_UPCALL, offset, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_4));
 	gen_insn(INSN_MOV, OP_SIZE_4, 0, 0);
-	gen_one(R_SCRATCH_NA_1);
+	gen_one(reg);
 	gen_address_offset();
+
+	return true;
+}
+
+static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsigned n_args)
+{
+	g(gen_get_upcall_pointer(ctx, offset, R_SCRATCH_NA_1));
 
 	gen_insn(INSN_CALL_INDIRECT, OP_SIZE_4, 0, 0);
 	gen_one(R_SCRATCH_NA_1);

c1-arm64.inc
@@ -502,12 +502,19 @@ static bool attr_w gen_upcall_argument(struct codegen_context attr_unused *ctx, 
 	return true;
 }
 
-static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsigned n_args)
+static bool attr_w gen_get_upcall_pointer(struct codegen_context *ctx, unsigned offset, unsigned reg)
 {
 	g(gen_address(ctx, R_UPCALL, offset, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_8));
 	gen_insn(INSN_MOV, OP_SIZE_8, 0, 0);
-	gen_one(R_SCRATCH_NA_1);
+	gen_one(reg);
 	gen_address_offset();
+
+	return true;
+}
+
+static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsigned n_args)
+{
+	g(gen_get_upcall_pointer(ctx, offset, R_SCRATCH_NA_1));
 
 	gen_insn(INSN_CALL_INDIRECT, OP_SIZE_8, 0, 0);
 	gen_one(R_SCRATCH_NA_1);

c1-hppa.inc
@@ -496,6 +496,16 @@ static bool attr_w gen_upcall_argument(struct codegen_context attr_unused *ctx, 
 	return true;
 }
 
+static bool attr_w gen_get_upcall_pointer(struct codegen_context *ctx, unsigned offset, unsigned reg)
+{
+	g(gen_address(ctx, R_UPCALL, offset, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_NATIVE));
+	gen_insn(INSN_MOV, OP_SIZE_ADDRESS, 0, 0);
+	gen_one(reg);
+	gen_address_offset();
+
+	return true;
+}
+
 static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsigned n_args)
 {
 #ifdef ARCH_PARISC32
@@ -503,18 +513,12 @@ static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsi
 	if (unlikely(!label))
 		return false;
 
-	g(gen_address(ctx, R_UPCALL, offset, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_NATIVE));
-	gen_insn(INSN_MOV, OP_SIZE_ADDRESS, 0, 0);
-	gen_one(R_SCRATCH_NA_1);
-	gen_address_offset();
+	g(gen_get_upcall_pointer(ctx, offset, R_SCRATCH_NA_1));
 
 	gen_insn(INSN_CALL, 0, 0, 0);
 	gen_four(label);
 #else
-	g(gen_address(ctx, R_UPCALL, offset, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_ADDRESS));
-	gen_insn(INSN_MOV, OP_SIZE_ADDRESS, 0, 0);
-	gen_one(R_DP);
-	gen_address_offset();
+	g(gen_get_upcall_pointer(ctx, offset, R_DP));
 
 	gen_insn(INSN_CALL_INDIRECT, OP_SIZE_8, 0, 0);
 	gen_one(R_DP);

c1-ia64.inc
@@ -564,12 +564,19 @@ static bool attr_w gen_upcall_argument(struct codegen_context attr_unused *ctx, 
 	return true;
 }
 
-static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsigned n_args)
+static bool attr_w gen_get_upcall_pointer(struct codegen_context *ctx, unsigned offset, unsigned reg)
 {
 	g(gen_address(ctx, R_UPCALL, offset, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_NATIVE));
 	gen_insn(INSN_MOV, OP_SIZE_ADDRESS, 0, 0);
-	gen_one(R_SCRATCH_NA_1);
+	gen_one(reg);
 	gen_address_offset();
+
+	return true;
+}
+
+static bool attr_w gen_upcall(struct codegen_context *ctx, unsigned offset, unsigned n_args)
+{
+	g(gen_get_upcall_pointer(ctx, offset, R_SCRATCH_NA_1));
 
 	g(gen_address(ctx, R_SCRATCH_NA_1, 0, IMM_PURPOSE_LDR_OFFSET, OP_SIZE_NATIVE));
 	gen_insn(INSN_MOV, OP_SIZE_ADDRESS, 0, 0);

commit c3017c10ea1b30c4daf404986e64f37f5e1e7d9e

parisc: fix delay slots in the conditional instructions
The polarity of "need_nop" was in fact correct and the commit
7ca91a8cf43ba32765cd261b591562fc8ba9429b broke it.

The problem was that when a function contains a conditional jump to the
immediatelly following instruction, cgen_cond_jmp would evaluate it as a
forward branch (and omit the delay slot), while the CPU would evaluate it
as a backward branch and apply the delay slot logic for backward
branches.

Fix this by rejecting branches to the following instruction in
resolve_relocation and fall back to JMP_SHORT which doesn't have this
problem.

c2-hppa.inc
@@ -735,7 +735,7 @@ static bool attr_w cgen_alu(struct codegen_context *ctx, unsigned size, unsigned
 		mc |= PARISC_N;
 		g(add_relocation(ctx, JMP_SHORTEST, ctx->code_position - arg1, &known));
 		g(cgen_2reg(ctx, mc, arg1[0], arg3[0]));
-		if (unlikely(!known))
+		if (unlikely(known))
 			cgen_four(PARISC_NOP);
 		return true;
 	}
@@ -1256,7 +1256,7 @@ static bool attr_w cgen_cond_jmp(struct codegen_context *ctx, uint32_t mc, unsig
 		mc |= PARISC_N;
 		g(add_relocation(ctx, JMP_SHORTEST, reloc_offset, &known));
 		g(cgen_2reg(ctx, mc, reg2, reg1));
-		if (unlikely(!known))
+		if (unlikely(known))
 			cgen_four(PARISC_NOP);
 	} else if (length == JMP_SHORT) {
 		mc |= PARISC_N;
@@ -1482,6 +1482,8 @@ static bool attr_w resolve_relocation(struct codegen_context *ctx, struct reloca
 	switch (reloc->length) {
 		case JMP_SHORTEST:
 			if (unlikely(offs < -0x800) || unlikely(offs >= 0x800))
+				return false;
+			if (unlikely(offs == -1))
 				return false;
 			memcpy(&mc, ctx->mcode + reloc->position, 4);
 			mc &= ~0x00001ffdU;

codegen.c
@@ -2181,6 +2181,7 @@ static bool attr_w cgen_entry(struct codegen_context *ctx)
 static bool attr_w cgen_label(struct codegen_context *ctx)
 {
 	uint32_t label_id = cget_four(ctx);
+	ajla_assert_lo(ctx->label_to_pos[label_id] == (size_t)-1, (file_line, "cgen_label: label already defined"));
 	ctx->label_to_pos[label_id] = ctx->mcode_size;
 	return true;
 }