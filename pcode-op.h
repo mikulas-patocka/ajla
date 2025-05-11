b62adda27594aa34a9edfb3b23d89d866aa8ed54
Mikulas Patocka <mikulas@twibright.com>
2025-05-11 18:27:38 +0200

    verify: introduce the flag '--verify=@' that will verify functions that have statements assume, claim or invariant in them
    make the function fill and sparse take the 'nat' argument
    
    add invariants to allocate_variables

--- pcode-op.h
+++ pcode-op.h
@@ -186,6 +186,7 @@
 #define Fn_IsFlatOption			8
 #define Fn_AlwaysFlatOption		16
 #define Fn_NoVerify			32
+#define Fn_VerifyTagged			64
 
 #define Local_Type_Record		0
 #define Local_Type_Option		1
