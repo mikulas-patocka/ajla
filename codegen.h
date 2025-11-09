ef76456db018da954ad333907f7b6458b0495852
Mikulas Patocka <mikulas@twibright.com>
2025-11-09 18:44:59 +0100

    ffi: support callbacks on power

--- codegen.h
+++ codegen.h
@@ -31,7 +31,7 @@ extern const char *dump_code;
 #define codegen_callback_init		name(codegen_callback_init)
 #define codegen_callback_done		name(codegen_callback_done)
 
-#if !defined(ARCH_PARISC) && !defined(ARCH_IA64) && !defined(ARCH_POWER)
+#if !defined(ARCH_PARISC) && !defined(ARCH_IA64)
 #define HAVE_CODEGEN_CALLBACK
 #endif
 
