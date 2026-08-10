mikulas-patocka/ajla

commit 4397cd49ef9eaf28d71dc749edcae41188348da9

save lazily called functions with multiple return values

data.c
@@ -678,7 +678,7 @@ static bool attr_fastcall thunk_alloc_result(struct thunk *t, arg_t n_return_val
 			thunk_free(t);
 			return false;
 		}
-		tm->u.multi_ret_reference.thunk = t;
+		tm->u.multi_ret_reference.thunk = pointer_thunk(t);
 		tm->u.multi_ret_reference.idx = ia;
 		result[ia] = tm;
 	}
@@ -1261,7 +1261,7 @@ static void * attr_hot_fastcall get_sub_multi_ret_reference(void *data)
 
 	address_unlock(t, DEPTH_THUNK);
 
-	mt = t->u.multi_ret_reference.thunk;
+	mt = pointer_get_thunk(t->u.multi_ret_reference.thunk);
 
 	address_lock(mt, DEPTH_THUNK);
 
@@ -1575,7 +1575,7 @@ process_result:
 	}
 
 	if (t_tag == THUNK_TAG_MULTI_RET_REFERENCE) {
-		struct thunk *mt = t->u.multi_ret_reference.thunk;
+		struct thunk *mt = pointer_get_thunk(t->u.multi_ret_reference.thunk);
 		tag_t mt_tag;
 		if (unlikely(!address_trylock_second(t, mt, DEPTH_THUNK))) {
 			address_unlock(t, DEPTH_THUNK);
@@ -1591,7 +1591,9 @@ process_result:
 		if (mt_tag == THUNK_TAG_RESULT) {
 			arg_t idx = t->u.multi_ret_reference.idx;
 			thunk_tag_set(t, THUNK_TAG_MULTI_RET_REFERENCE, THUNK_TAG_RESULT);
+			t->u.function_call.n_results = 1;
 			t->u.function_call.results[0].ptr = mt->u.function_call.results[idx].ptr;
+			mt->u.function_call.results[idx].wanted = false;
 			pointer_poison(&mt->u.function_call.results[idx].ptr);
 			if (thunk_dereference_nonatomic(mt)) {
 				address_unlock_second(t, mt, DEPTH_THUNK);
@@ -3331,6 +3333,22 @@ static bool attr_fastcall save_result(void *data, uintptr_t attr_unused offset, 
 	return true;
 }
 
+static bool attr_fastcall save_multi_ret_reference(void *data, uintptr_t attr_unused offset, size_t attr_unused *align, size_t *size, struct stack_entry **subptrs, size_t *subptrs_l)
+{
+	ajla_error_t sink;
+	struct thunk *t = data;
+
+	*size = partial_sizeof_array(struct thunk, u.function_call.results, 1);
+
+	*subptrs = mem_alloc_mayfail(struct stack_entry *, sizeof(struct stack_entry), &sink);
+	if (unlikely(!*subptrs))
+		return false;
+	(*subptrs)[0].t = &save_pointer;
+	(*subptrs)[0].ptr = &t->u.multi_ret_reference.thunk;
+	*subptrs_l = 1;
+	return true;
+}
+
 static bool attr_fastcall save_exception(void *data, uintptr_t offset, size_t attr_unused *align, size_t *size, struct stack_entry **subptrs, size_t *subptrs_l)
 {
 	ajla_error_t sink;
@@ -3558,6 +3576,7 @@ void name(data_init)(void)
 	data_method_table[DATA_TAG_saved_cache].save = save_saved_cache;
 	data_method_table[THUNK_TAG_FUNCTION_CALL].save = save_function_call;
 	data_method_table[THUNK_TAG_RESULT].save = save_result;
+	data_method_table[THUNK_TAG_MULTI_RET_REFERENCE].save = save_multi_ret_reference;
 	data_method_table[THUNK_TAG_EXCEPTION].save = save_exception;
 
 	oom = thunk_alloc_exception_mayfail(error_ajla(EC_ASYNC, AJLA_ERROR_OUT_OF_MEMORY), NULL pass_file_line);

data.h
@@ -1385,7 +1385,7 @@ struct thunk {
 		} function_call;
 		/* THUNK_TAG_MULTI_RET_REFERENCE */
 		struct {
-			struct thunk *thunk;
+			pointer_t thunk;
 			arg_t idx;
 		} multi_ret_reference;
 		/* THUNK_TAG_EXCEPTION */
@@ -1462,7 +1462,7 @@ static inline bool thunk_is_finished(struct thunk *t)
 {
 	tag_t tag = thunk_tag_volatile(t);
 	return   tag == THUNK_TAG_RESULT ||
-		(tag == THUNK_TAG_MULTI_RET_REFERENCE && thunk_tag_volatile(t->u.multi_ret_reference.thunk) == THUNK_TAG_RESULT);
+		(tag == THUNK_TAG_MULTI_RET_REFERENCE && thunk_tag_volatile(pointer_get_thunk(t->u.multi_ret_reference.thunk)) == THUNK_TAG_RESULT);
 }
 
 static inline struct thunk *thunk_pointer_tag(struct thunk *t)
@@ -1752,11 +1752,14 @@ static inline void thunk_reference(struct thunk *t)
 
 static inline void thunk_reference_nonatomic(struct thunk *t)
 {
-	refcount_inc_nonatomic(&t->refcount_);
+	if (likely(!refcount_is_read_only(&t->refcount_)))
+		refcount_inc_nonatomic(&t->refcount_);
 }
 
 static inline bool thunk_dereference_nonatomic(struct thunk *t)
 {
+	if (likely(refcount_is_read_only(&t->refcount_)))
+		return false;
 	return refcount_dec_nonatomic(&t->refcount_);
 }
 
@@ -1767,6 +1770,8 @@ static inline bool thunk_refcount_is_one_nonatomic(struct thunk *t)
 
 static inline refcount_int_t thunk_refcount_get_nonatomic(struct thunk *t)
 {
+	if (likely(refcount_is_read_only(&t->refcount_)))
+		return 0;
 	return refcount_get_nonatomic(&t->refcount_);
 }
 

commit 1f977165f5c1776ed9db12e2509c86851b251f30

fix random crashes introduced by 4397cd49ef9eaf28d71dc749edcae41188348da9
The line "mt->u.function_call.results[idx].wanted = false" was buggy
because it could race with the do-while loop in thunk_terminate.
thunk_terminate would call pointer_dereference on a live pointer.

data.c
@@ -828,7 +828,7 @@ return_dereference_unused:
 		do {
 			if (n_return_values == 1 || !t->u.function_call.results[i].wanted) {
 				pointer_dereference(t->u.function_call.results[i].ptr);
-				pointer_poison(&t->u.function_call.results[i].ptr);
+				t->u.function_call.results[i].ptr = pointer_empty();
 			}
 		} while (++i < n_return_values);
 		address_lock(t, DEPTH_THUNK);
@@ -1593,8 +1593,7 @@ process_result:
 			thunk_tag_set(t, THUNK_TAG_MULTI_RET_REFERENCE, THUNK_TAG_RESULT);
 			t->u.function_call.n_results = 1;
 			t->u.function_call.results[0].ptr = mt->u.function_call.results[idx].ptr;
-			mt->u.function_call.results[idx].wanted = false;
-			pointer_poison(&mt->u.function_call.results[idx].ptr);
+			mt->u.function_call.results[idx].ptr = pointer_empty();
 			if (thunk_dereference_nonatomic(mt)) {
 				address_unlock_second(t, mt, DEPTH_THUNK);
 				address_unlock(t, DEPTH_THUNK);
@@ -3321,7 +3320,7 @@ static bool attr_fastcall save_result(void *data, uintptr_t attr_unused offset, 
 		if (unlikely(!array_init_mayfail(struct stack_entry, subptrs, subptrs_l, &sink)))
 			return false;
 		for (i = 0; i < n_return_values; i++) {
-			if (t->u.function_call.results[i].wanted) {
+			if (t->u.function_call.results[i].wanted && !pointer_is_empty(t->u.function_call.results[i].ptr)) {
 				struct stack_entry ste;
 				ste.t = &save_pointer;
 				ste.ptr = &t->u.function_call.results[i].ptr;